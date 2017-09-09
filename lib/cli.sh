c5294737069fe05d78c1a94650084cea319a6b6c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-09-09 10:03:44 -0400

    checkpoint

---
+++ lib/cli.sh
@@ -0,0 +1,49 @@
+p6_git_init() {
+
+    git init "$@"
+}
+
+p6_git_checkout() {
+
+    git checkout "$@"
+}
+
+p6_git_reset() {
+
+    git reset "$@"
+}
+
+p6_git_commit() {
+
+    git commit "$@"
+}
+
+p6_git_add() {
+
+    git add "$@"
+}
+
+p6_git_status() {
+
+    git status "$@"
+}
+
+p6_git_push() {
+
+    git push "$@"
+}
+
+p6_git_remote() {
+
+    git remote "$@"
+}
+
+p6_git_symbolic_ref() {
+
+    git symbolic-ref "$@"
+}
+
+p6_git_rev_parse() {
+
+    git rev-parse "$@"
+}
---
+++ lib/util.sh
@@ -0,0 +1,32 @@
+p6_git_branch_get() {
+
+    local str=$(p6_git_symbolic_ref "HEAD")
+
+    local branch=${str#refs/head}
+
+    echo $branch
+}
+
+p6_git_org_repo_get() {
+
+    p6_git_remote "remote-url" "origin"
+	cut -d: -f 2 | \
+	sed -e 's,\.git$,,'
+}
+
+p6_git_sha_short_get() {
+
+    p6_git_rev_parse "--short" "HEAD"
+}
+
+p6_git_dirty_get() {
+
+    local gstatus="$(p6_git_status "--porcelain" 2>/dev/null | tail -1)"
+    if [ -n "$gstatus" ]; then
+	echo 1
+	return 1
+    else
+	echo 0
+	return 0
+    fi
+}
