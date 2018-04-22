fd9a9fdd9f5e180da0a2d40dbf87a2bc68918b03
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:35:50 -0400

    brew

---
+++ init.zsh
@@ -0,0 +1,15 @@
+p6df::modules::p6git::version() { echo "0.0.1" }
+p6df::modules::p6git::deps()    { }
+p6df::modules::p6git::external() { }
+
+p6df::modules::p6git::init() {
+
+    local dir=$P6_DFZ_DATA_PARENT/p6m7g8/p6git
+
+    local file
+    for file in $dir/lib/*.sh; do
+        P6_DEBUG=1 p6_file_load "$file"
+    done
+}
+
+p6df::modules::p6git::init
