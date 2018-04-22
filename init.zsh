99b3509e450292c26ce49325bd1656abd96f8005
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-22 00:01:21 -0400

    brew

--- init.zsh
+++ init.zsh
@@ -1,6 +1,6 @@
 p6df::modules::p6git::version() { echo "0.0.1" }
 p6df::modules::p6git::deps()    { }
-p6df::modules::p6git::external() { }
+p6df::modules::p6git::external::brew() { }
 
 p6df::modules::p6git::init() {
 
