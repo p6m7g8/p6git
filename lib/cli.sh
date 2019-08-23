80ec81e31ef7c8359c315ddc28269fca1f992e42
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-08-23 13:22:35 -0400

    renames

--- lib/cli.sh
+++ lib/cli.sh
@@ -1,49 +1,49 @@
 p6_git_init() {
 
-    p6_log_or_run git init "$@"
+    p6_run_write_cmd git init "$@"
 }
 
 p6_git_checkout() {
 
-    p6_log_or_run git checkout "$@"
+    p6_run_write_cmd git checkout "$@"
 }
 
 p6_git_reset() {
 
-    p6_log_or_run git reset "$@"
+    p6_run_write_cmd git reset "$@"
 }
 
 p6_git_commit() {
 
-    p6_log_or_run git commit "$@"
+    p6_run_write_cmd git commit "$@"
 }
 
 p6_git_add() {
 
-    p6_log_or_run git add "$@"
+    p6_run_write_cmd git add "$@"
 }
 
 p6_git_status() {
 
-    p6_log_and_run git status "$@"
+    p6_run_read_cmd git status "$@"
 }
 
 p6_git_push() {
 
-    p6_log_or_run git push "$@"
+    p6_run_write_cmd git push "$@"
 }
 
 p6_git_remote() {
 
-    p6_log_or_run git remote "$@"
+    p6_run_write_cmd git remote "$@"
 }
 
 p6_git_symbolic_ref() {
 
-    p6_log_and_run git symbolic-ref "$@"
+    p6_run_read_cmd git symbolic-ref "$@"
 }
 
 p6_git_rev_parse() {
 
-    p6_log_and_run git rev-parse "$@"
+    p6_run_read_cmd git rev-parse "$@"
 }
