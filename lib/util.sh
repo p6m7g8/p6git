dedce3a82864b06c3a4e77f174fee161479275bd
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-10-04 19:20:30 -0400

    return

--- lib/util.sh
+++ lib/util.sh
@@ -14,7 +14,7 @@ p6_git_branch_get() {
     local str=$(p6_git_symbolic_ref "HEAD")
     local branch=${str#refs/heads/}
 
-    p6_return "$branch"
+    p6_return_str "$branch"
 }
 
 ######################################################################
@@ -33,7 +33,7 @@ p6_git_org_repo_get() {
     local url=$(p6_git_remote "get-url" "origin" 2>&1)
     local repo=${${url##*/}%.git}
 
-    p6_return "$repo"
+    p6_return_str "$repo"
 }
 
 ######################################################################
@@ -52,7 +52,7 @@ p6_git_org_org_get() {
     local url=$(p6_git_remote "get-url" "origin" 2>&1)
     local org=${${url%/*}##*/}
 
-    p6_return "$org"
+    p6_return_str "$org"
 }
 
 ######################################################################
@@ -80,5 +80,7 @@ p6_git_dirty_get() {
 
     local gstatus="$(p6_git_status "--porcelain" 2>/dev/null | tail -1)"
 
-    p6_string_blank "$gstatus" && p6_return_bool 1 || p6_return_bool 0
+    local rc=$(p6_string_blank "$gstatus")
+
+    p6_return_bool "$rc"
 }
